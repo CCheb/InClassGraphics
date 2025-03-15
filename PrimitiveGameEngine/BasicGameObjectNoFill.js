6063d66fec01a38fa914a656f5979b93e419a177
Sebastian <amsebastian12@gmail.com>
2025-03-15 16:22:38 -0400

    updated the files to be the latest version

--- PrimitiveGameEngine/BasicGameObjectNoFill.js
+++ PrimitiveGameEngine/BasicGameObjectNoFill.js
@@ -55,7 +55,7 @@ class GameObject
 		this.loc = [0,0,0];
 		this.rot = [0,0,0];
 		this.isTrigger = false;
-		this.collissionRadius = 1.0;
+		this.collissionRadius = 0.1; //this by default may be too large
 		this.velocity = [0,0,0];
 		this.angVelocity = [0,0,0];
 		this.name = "Default";
@@ -63,16 +63,97 @@ class GameObject
 		this.tranform = new Transform();
 		this.prefab;
 	}
+
+	//assuming that velocity is set correctly 
+	Move(){
+		var tempP = [0,0,0]
+		for (var i = 0;  i < 4; i++){
+			tempP[i] = this.loc[i]
+			tempP[i] = this.velocity[i]
+			this.rot[i] += this.angVelocity[i]
+		}
+		if(!this.isTrigger){
+			var clear = true;
+			for(var so in m.Solid)
+				{ // what is this line
+				if(m.CheckCollision(tempP, this.collissionRadius,m.Solid[so].loc,m.Solid[so].collissionRadius))//make sure m.solid does not equal this object
+					{
+					OnCollisionEnter(m.Solid[so])
+					try
+					{
+						m.Solid[so].OnCollisionEnter(this)
+					}
+					catch{}
+					clear = false
+				}
+			}
+		}
+		if(clear){
+			this.loc = tempP;
+		}
+		else{ //this should be right 
+			this.loc = tempP;
+			for(var so in m.Solid){
+				if(m.CheckCollision(tempP, this.collissionRadius,m.Solid[so].loc,m.Solid[so].collissionRadius)){
+					this.OnTriggerEnter(m.Solid[so])
+					try
+					{
+						m.Solid[so].OnTriggerEnter(this)
+					}
+					catch
+					{
+
+					}
+				}
+
+			}
+			for(var so in m.Trigger){ //this should be correct. It is trying to check for tigger objects insted of solid objects
+				if(this != m.Trigger[so]){
+					if(m.CheckCollision(tempP, this.collissionRadius,m.Trigger[so].loc,m.Trigger[so].collissionRadius)){
+						this.OnTriggerEnter(m.Solid[so])
+						try
+						{
+							m.Trigger[so].OnTriggerEnter(this)
+						}
+						catch
+						{
 	
+						}
+					}
+				}
+				
+				
+			}
+		}
+	} 
+
+
+	//virtural functions 
+	//colide with a phyical object and it stops me 
+	OnCollisionEnter(other){
+
+	}
+
+	//colide with a object and a event happens
+	OnTriggerEnter(other){
+
+	}
+
+
+	//make a fake abstract class
 	Update()
 	{
 		console.error(this.name +" update() is NOT IMPLEMENTED!");
 	}
-	Render()
+	Render(program) //needs this.program
 	{
+		//could possibly impliment render here
 		console.error(this.name + " render() is NOT IMPLEMENTED!");
 	}	
 }
+
+
+//shape extends game object 
 
 class Demo extends GameObject
 {
